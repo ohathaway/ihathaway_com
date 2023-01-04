bee238632e4c42bca3c9ac0d6f59bc2e97849e0f
Owen Hathaway <owen@prsdata.com>
2023-01-04 13:32:29 -0700

    added copy to the Technologist section

--- components/TheTechnologist.vue
+++ components/TheTechnologist.vue
@@ -60,6 +60,25 @@
           Wiktionary</a> as "A person who uses science or technology to solve
           practical problems."
         </p>
+        <p>
+          Early humans used stone tools, such as arrow heads, spear tips,
+          knives, scrapers, and such. These crude implements probably represent
+          the earliest use of technology by our species. By learning to
+          control fire and using language to share knowledge, use of technology
+          may be the core element of what separated humans from the rest of the
+          animal kingdom.
+        </p>
+        <p>
+          Rather than adopting the
+          <a href="https://en.wikipedia.org/wiki/Luddite"
+             alt="Early Eighteenth Century Secret Society - the Luddites"
+          >Luddites'</a> approach to technology, I approach problem solving
+          using the best tool I have for the job. I am a walking stereotype when
+          it comes to guys and gadgets, but that fascination affords me the
+          ability to bring a diverse array of tools to many different types of
+          problems. Tools are cool. Humans are cooler. Put the two together, and
+          we accomplish incredible things.
+        </p>
       </div>
     </div>
   </section>

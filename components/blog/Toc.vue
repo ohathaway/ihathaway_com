ae7697e956ebd7d51599242d4ef18545204ff5a6
Owen Hathaway <owen@prsdata.com>
2023-01-17 10:56:13 -0700

    started Toc component

---
+++ components/blog/Toc.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script setup>
+import { flattenDeep } from 'lodash'
+
+const props = defineProps(['links'])
+
+const flatLinks = flattenDeep(props.links).map( link => {
+  console.debug('link: ', link)
+  let _link = [link]
+  if (link.children) {
+    console.debug(`${link.id} has ${link.children.length} children`)
+    const flattened =  flattenDeep(link.children)
+    _link = [link, ...flattened]
+  }
+  return _link
+}).flat(1)
+</script>
---
+++ content/blog/a-few-belly-buttons.md
@@ -0,0 +1,11 @@
+---
+title: A Few Belly Butotns
+description: A second post, maybe with some content later
+img: 'img/cover (1).JPG'
+tags: [Owen Hathaway, Manifesto, belly-button]
+---
+# Everyone has a belly button, some have two
+
+David
+
+Julian
